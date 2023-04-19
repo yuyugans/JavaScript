392b11341eaf4d72092b5ca9ab9fa5fc94e802b0
yuyugans <mohamadyusupaxioo@gmail.com>
2023-04-19 17:09:06 +0700

    "afrizal tutorial"

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,1 +1,35 @@
-document.writeln("ngetes ae");
+//tugas kecil
+
+//no 1
+var saldo = prompt('Masukan saldo')
+alert(saldo)
+
+//no 2
+var hari = new Date().getDay()
+switch (hari){
+    case 1 :
+        hari = "hari ini adalah hari senin"
+    break
+    case 2 :
+        hari = "hari ini adalah hari selasa"
+    break
+    case 3 :
+        hari = "hari ini adalah hari rabu"
+    break
+    case 4 :
+        hari = "hari ini hari kamis"
+    break
+    case 5 :
+        hari = "hari ini hari jumat"
+    break
+    case 6 :
+        hari = "hari ini hari sabtu"
+    break
+    case 7 :
+        hari = "hari ini hari minggu"
+    break
+    default : hari = "gada"
+}
+
+alert(hari)
+
